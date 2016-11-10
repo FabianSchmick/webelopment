4a451f405fe0307509aae28055b226c86ca7ef42
Fabian Schmick <schmick.fabian@web.de>
2016-11-10 21:04:22 +0100

    Added btn and nav styles.

---
+++ doc/examples/navigation.examples.php
@@ -0,0 +1,36 @@
+<?php
+/* ---------------------------------------------------------------- */
+/*                		    Drop Down Menu	                        */
+/* ---------------------------------------------------------------- */
+?>
+
+<!-- In pages/frontend/inc/header -->
+<nav class="nav">
+	<ul>
+		<li class="active"><a href="#">Home</a></li>
+		<li><a href="#">Menu 1</a>
+			<ul>
+    			<li><a href="#">Sub Menu 1</a></li>
+      			<li><a href="#">Sub Menu 2</a></li>
+      		</ul>
+  		</li>
+  		<li><a href="#">Menu 2</a>
+			<ul>
+    			<li><a href="#">Sub Menu 1</a></li>
+      			<li><a href="#">Sub Menu 2</a>
+      				<ul>
+			          <li><a href="#">Deep Menu 1</a>
+			            <ul>
+			              <li><a href="#">Sub Deep 1</a></li>
+			              <li><a href="#">Sub Deep 2</a></li>
+			              <li><a href="#">Sub Deep 3</a></li>
+			                <li><a href="#">Sub Deep 4</a></li>
+			            </ul>
+			          </li>
+			          <li><a href="#">Deep Menu 2</a></li>
+			        </ul>
+    			</li>
+      		</ul>
+  		</li>
+	</ul>
+</nav>
