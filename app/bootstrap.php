5c53cb0c15e9a6c788996fd506c38c6148a9405b
Fabian Schmick <schmick.fabian@web.de>
2017-03-04 17:16:54 +0100

    Application class handles the project.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -7,31 +7,14 @@ if (file_exists(__DIR__ . "/../vendor/autoload.php")) {
 	require_once __DIR__ . "/../vendor/autoload.php";
 }
 
-// Unordered functions
-include_once __DIR__ . "/functions/functions.inc.php";
-
-use database\PDODatabase;
+use application\Application;
 use application\Config;
 
 
+$app = new Application();
+
+$app->run();
+$app->includeFiles(['functions' => ["functions.inc.php"]]);
+
+// Global config object
 $config = new Config('config.inc.php');
-
-// Display debug messages
-if ($config->debug) {
-	error_reporting(E_ALL);
-	ini_set("display_errors", 1);
-} else {
-	error_reporting(0);
-}
-
-// Database object initialization
-if (isset($config->dbType) &&
-	isset($config->dbHost) &&
-	isset($config->dbName) &&
-	isset($config->dbUser) &&
-	isset($config->dbPassword)) {
-
-	$pdoDb = new PDODatabase($config->dbType, $config->dbHost, $config->dbName, $config->dbUser, $config->dbPassword);
-
-	$connection = $pdoDb->initializePDOObject();
-}
---
+++ app/classes/application/application.class.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace application;
+
+class Application
+{
+    /*
+     * Config array
+     */
+    private $config = [];
+
+
+    public function __construct()
+    {
+        $this->config = new Config('config.inc.php');
+    }
+
+    // Runs the application
+    public function run()
+    {
+        $this->checkDebugMode();
+    }
+
+    // Sets Error Reporting
+    public function checkDebugMode()
+    {
+        // Display debug messages
+        if ($this->config->debug) {
+            error_reporting(E_ALL);
+            ini_set("display_errors", 1);
+        } else {
+            error_reporting(0);
+        }
+    }
+
+    // Include files
+    public function includeFiles($files = [])
+    {
+        foreach($files as $folder => $file_array) { 	// Loop Array
+            foreach($file_array as $file) {
+                include_once __DIR__ . "/../../" . $folder . "/" . $file; 	// Include files
+            }
+        }
+    }
+
+    // Database object initialization
+    public function initDbConnection()
+    {
+        if (!empty($this->config->dbType) &&
+            !empty($this->config->dbHost) &&
+            !empty($this->config->dbName) &&
+            !empty($this->config->dbUser) &&
+            isset($this->config->dbPassword)) {
+
+            $pdoDb = new \database\PDODatabase(
+                $this->config->dbType,
+                $this->config->dbHost,
+                $this->config->dbName,
+                $this->config->dbUser,
+                $this->config->dbPassword
+            );
+
+            $connection = $pdoDb->initializePDOObject();
+        } else {
+            $connection = "Error - no database connection configured.";
+        }
+
+        return $connection;
+    }
+}
